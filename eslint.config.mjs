8e437cf3ef6112b4b48c06f444deed59939284cd
Guido Pérez Moro <guidoperezmoro@gmail.com>
2025-06-09 15:13:12 -0300

    Prettier Consistency

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,25 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-import prettier from "eslint-plugin-prettier";
-import prettierConfig from "eslint-config-prettier";
+import {dirname} from 'path'
+import {fileURLToPath} from 'url'
+import {FlatCompat} from '@eslint/eslintrc'
+import prettier from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
   prettierConfig, // Disables conflicting ESLint rules
   {
-    plugins: { prettier },
+    plugins: {prettier},
     rules: {
-      "prettier/prettier": "warn", // Show warnings for Prettier issues
+      'prettier/prettier': 'warn', // Show warnings for Prettier issues
     },
   },
-];
+]
 
-export default eslintConfig;
+export default eslintConfig
