7ddf7d9fc7d126fe83fb9fe7b475d85884d875a7
Guido Pérez Moro <guidoperezmoro@gmail.com>
2025-07-10 15:49:48 -0300

    Endpoints en minúsucla

--- src/services/empresa.ts
+++ src/services/empresa.ts
@@ -3,25 +3,26 @@ import {Empresa} from './types'
 
 // Obtener todas las empresas
 export async function fetchAllEmpresas(): Promise<Empresa[]> {
-  const response = await api.get<Empresa[]>('/Empresas')
+  const response = await api.get<Empresa[]>('/empresas')
   return response.data
 }
 
 // Obtener una empresa por ID
 export async function fetchEmpresaById(id: number): Promise<Empresa> {
-  const response = await api.get<Empresa>(`/Empresas/${id}`)
+  const response = await api.get<Empresa>(`/empresas/${id}`)
   return response.data
 }
 
 // Crear nueva empresa
 export async function createEmpresa(data: Omit<Empresa, 'id'>): Promise<Empresa> {
-  const response = await api.post<Empresa>('/Empresas', data)
+  const response = await api.post<Empresa>('/empresas', data)
+  // imagen? No. El Nico va a agregarlo al constructor
   return response.data
 }
 
 // Actualizar empresa
 export async function updateEmpresa(id: number, data: Partial<Empresa>): Promise<Empresa> {
-  const response = await api.put<Empresa>(`/Empresas/${id}`, data)
+  const response = await api.put<Empresa>(`/empresas/${id}`, data)
   return response.data
 }
 
