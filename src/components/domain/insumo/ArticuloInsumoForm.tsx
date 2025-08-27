836efb77c7e669422a959ad05d5aa1c119e2ef00
Guido Pérez Moro <guidoperezmoro@gmail.com>
2025-08-27 17:33:51 -0300

    Removed unused import

--- src/components/domain/insumo/ArticuloInsumoForm.tsx
+++ src/components/domain/insumo/ArticuloInsumoForm.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import {useEffect, useMemo, useState} from 'react'
+import {useEffect, useState} from 'react'
 import Input from '@/components/ui/Input'
 import Toggle from '@/components/ui/Toggle'
 import Dropdown from '@/components/ui/Dropdown'
